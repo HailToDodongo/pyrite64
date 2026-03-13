5c256a4443dc9a7d2b7e8d023af40ea9a174d82d
Nolan Baker <nolan.lee.baker@gmail.com>
2026-03-13 03:40:25 -0500

    fix: caches project data so I'm not thrashing the disk

--- src/editor/pages/launcher.h
+++ src/editor/pages/launcher.h
@@ -3,11 +3,19 @@
 * @license MIT
 */
 #pragma once
+#include <vector>
 #include "SDL3/SDL_gpu.h"
 #include "../../renderer/texture.h"
 
 namespace Editor
 {
+  struct ProjectEntry {
+    std::string name;
+    std::string path;
+    std::string editorVersion;
+    std::string lastModified;
+  };
+
   class Launcher
   {
     private:
@@ -16,11 +24,14 @@ namespace Editor
       Renderer::Texture texBtnOpen;
       Renderer::Texture texBtnTool;
       Renderer::Texture texBG;
+      std::vector<ProjectEntry> projectEntries;
 
     public:
       Launcher(SDL_GPUDevice* device);
       ~Launcher();
 
       void draw();
+      void showContextMenu(const std::string& path);
+
   };
 }
